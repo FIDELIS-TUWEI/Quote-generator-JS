420d8e3621a67ba336fe98cd3c5efdd290411a94
Fidelis <fideliofidel9@gmail.com>
2023-01-20 19:17:09 +0300

    app complete

--- PROJECT/JS/index.js
+++ PROJECT/JS/index.js
@@ -31,8 +31,9 @@ buttonContainer.setAttribute('class', 'button__container');
 quoteContainer.appendChild(buttonContainer);
 
 // Button
-const twitterBtn = document.createElement('button');
+const twitterBtn = document.createElement('img');
 twitterBtn.setAttribute('class', 'twitter__button');
+twitterBtn.setAttribute('src', './image/twitter.svg')
 twitterBtn.setAttribute('id', 'twitter');
 twitterBtn.setAttribute('title', 'Tweet This!');
 buttonContainer.appendChild(twitterBtn);
@@ -43,10 +44,10 @@ twitterIcon.setAttribute('class', '<i class="fa-brands fa-twitter"></i>');
 twitterBtn.appendChild(twitterIcon);
 
 // New Quote Button
-const NewQuoteBtn = document.createElement('button');
-NewQuoteBtn.setAttribute('id', 'new__quote');
-NewQuoteBtn.innerText = `New Quote`;
-buttonContainer.appendChild(NewQuoteBtn);
+const newQuoteBtn = document.createElement('button');
+newQuoteBtn.setAttribute('id', 'new__quote');
+newQuoteBtn.innerText = `New Quote`;
+buttonContainer.appendChild(newQuoteBtn);
 
 
 // Loader div
@@ -54,3 +55,83 @@ const loaderContainer = document.createElement('div');
 loaderContainer.setAttribute('class', 'loader');
 loaderContainer.setAttribute('id', 'loader');
 document.body.append(loaderContainer);
+
+
+
+
+// GET THE ELEMENT FROM THE DOM
+const quoteMainContainer = document.getElementById('quote__container');
+const quoteTexts = document.getElementById('quote');
+const author = document.getElementById('author');
+const twitter = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new__quote');
+const loader = document.getElementById('loader');
+
+// Declare Quotes from API, store in avariable and assign an empty array
+let apiQuotes = [];
+
+// Loading spinner function
+loading = () => {
+    loader.hidden = false;
+    quoteMainContainer.hidden = true;
+}
+
+// Function to remove loading spinner
+complete = () => {
+    quoteMainContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// Function to Display new Quote
+newQuote = () => {
+    loading();
+
+    // Pick a random Quote from the array
+    const randomQuote = apiQuotes[Math.floor( Math.random()* apiQuotes.length)];
+     
+    // Check if author field is empty and replace with 'Unknown
+    if (!randomQuote.author) {
+        author.textContent = 'Unknown';
+    } else {
+        author.textContent = randomQuote.author;
+    }
+
+    // Check quote length to determine styling
+    if (randomQuote.text.length > 120) {
+        quoteTexts.classList.add('long__quote');
+    } else {
+        quoteTexts.classList.remove('long__quote');
+    }
+
+    // Set quote and hide loader
+    quoteTexts.textContent = randomQuote.text;
+    complete();
+}
+
+ // Fetch quotes from API
+ async function getQuotes() {
+    loading();
+
+    try {
+        const response = await fetch('https://type.fit/api/quotes');
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+// Tweet Quote Function
+tweetQuote = () => {
+    const twitterUrl = `https://twitter.com/intent/tweet?text= ${quoteTexts.innerText} - ${author.innerText}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Events
+newQuoteButton.addEventListener('click', newQuote);
+twitter.addEventListener('click', tweetQuote);
+console.log(twitterBtn);
+
+// On Load
+getQuotes();
